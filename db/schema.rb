c38e464e844a45fb33126d6412d4c38638e4f2a5
cdyap <cdyap@outlook.com>
2017-05-17 12:19:54 +0800

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -69,8 +69,8 @@ ActiveRecord::Schema.define(version: 20170516152015) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
