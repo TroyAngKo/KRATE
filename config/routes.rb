385ebdbb868e33d763c90691413fcabcde6a56ae
cdyap <cdyap@outlook.com>
2017-05-17 22:37:08 +0800

    Fixed dropdown and added profile route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'startup/get_background'
   get 'startup/get_username'
   get 'startup/get_email'
+  get 'users/profile'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
