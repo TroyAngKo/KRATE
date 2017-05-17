cab9003c759cbde363eb17343773eee292031a99
cdyap <cdyap@outlook.com>
2017-05-17 22:58:08 +0800

    birthday

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@ class User < ActiveRecord::Base
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
     user.name = auth.info.name   # assuming the user model has a name
-    user.birthday = Date.parse(auth.info.birthday)
+    if !auth.info.birthday.nil?
+      user.birthday = Date.parse(auth.info.birthday)
     # If you are using confirmable and the provider(s) you use validate emails, 
     # uncomment the line below to skip the confirmation emails.
     # user.skip_confirmation!
